8a945b852b790e765a9d9f9e62b9dc9fd53ee8cd
ERISCHA24 <2410817120022@mhs.ulm.ac.id>
2025-11-05 21:12:46 +0800

    Add files via upload

---
+++ MODUL 5/src/Soal1/Shape.java
@@ -0,0 +1,16 @@
+package Soal1;
+
+public abstract class Shape {
+    private String shapeName;
+
+    public Shape(String name) {
+        shapeName = name;
+    }
+
+    public abstract double area();
+
+    public String toString() {
+        return shapeName;
+    }
+
+}
